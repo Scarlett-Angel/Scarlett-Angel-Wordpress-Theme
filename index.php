efc39085ee735307ed619c5a292dbbd84c360b13
debiruman665 <angelpromos@gmail.com>
2016-03-20 19:19:59 +0000

    Stephen Update
    Tidied up index.php
    Moved word animator function to plugin repo
    fixed add wp_footer on splash-template.php

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
 get_header();
-if(have_posts()) :
-while (have_posts()) : the_post(); ?>
-<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-<?php $string = "This has many words This has many words This has many words This has many words This has many words This has many words " ?>
-<?php wordEffects($string, 0, 15, 0, array("style1","style2"), "something:thing;", "else:thing;"); ?>
-<?php the_content(); ?>
-<?php endwhile;
-else :
-echo 'nothing';
+if (have_posts()):
+    while (have_posts()):
+        the_post();
+?>
+<h2><a href="<?php
+        the_permalink();
+?>"><?php
+        the_title();
+?></a></h2>
+<?php
+        $string = "This has many words This has many words This has many words This has many words This has many words This has many words "?>
+<?php
+        wordEffects($string, 0, 15, 0, array(
+            "style1",
+            "style2"
+        ), "something:thing;", "else:thing;");
+?>
+<?php
+        the_content();
+?>
+<?php
+    endwhile;
+else:
+    echo 'nothing';
 endif;
 get_footer();
 ?>
--- splash-template.php
+++ splash-template.php
@@ -2,10 +2,20 @@
 /*
 Template Name: Splash Page Template
 */
-
-
-
-get_footer();
-
-
 ?>
+<head>
+    <?php wp_head(); ?>
+
+</head>
+
+<?php
+if (is_active_sidebar('Splash Title')){
+    
+    dynamic_sidebar('Splash Title');
+    echo "hey";
+}
+?>
+
+<?php get_footer(); ?>
+
+
