3900488fe7fc037ae948ebd7c4358c5086120f13
debiruman665 <angelpromos@gmail.com>
2016-08-01 06:11:14 +0100

    Fixed the menu burger

--- bottom-corner.php
+++ bottom-corner.php
@@ -5,40 +5,40 @@ Template Name: Bottom Corner Logo
 ?>
 <style>
     .bottom-corner {background-image:url(<?php
-    echo get_template_directory_uri() . "/theme-images/";
-    switch (rand(1,10)){
-        case 1:
-            echo "1.gif";
-            break;
-        case 2:
-            echo "2.gif";
-            break;
-        case 3:
-            echo "3.gif";
-            break;
-        case 4:
-            echo "4.gif";
-            break;
-        case 5:
-            echo "5.gif";
-            break;
-        case 6:
-            echo "6.gif";
-            break;
-        case 7:
-            echo "7.gif";
-            break;
-        case 8:
-            echo "8.gif";
-            break;
-        case 9:
-            echo "9.gif";
-            break;
-        case 10:
-            echo "10.gif";
-            break;
-    }
-    ?>); background-size: cover; background-attachment:fixed; background-repeat: no-repeat;}
+echo get_template_directory_uri() . "/theme-images/";
+switch(rand(1, 10)) {
+	case 1:
+		echo "1.gif";
+		break;
+	case 2:
+		echo "2.gif";
+		break;
+	case 3:
+		echo "3.gif";
+		break;
+	case 4:
+		echo "4.gif";
+		break;
+	case 5:
+		echo "5.gif";
+		break;
+	case 6:
+		echo "6.gif";
+		break;
+	case 7:
+		echo "7.gif";
+		break;
+	case 8:
+		echo "8.gif";
+		break;
+	case 9:
+		echo "9.gif";
+		break;
+	case 10:
+		echo "10.gif";
+		break;
+} //rand(1, 10)
+?>); background-size: cover; background-attachment:fixed; background-repeat: no-repeat;}
 </style>
 <?php
 get_header();
@@ -48,15 +48,17 @@ get_header();
 <div id="main-content-area" class="col-lg-10 col-md-10 col-md-push-2">
     <div id="main-inner-text" class="col-md-10">
     <div class="content-float"><?php
-if (have_posts()):
+if(have_posts()):
 ?>
 <?php
-    while (have_posts()):
-        the_post();
+	while(have_posts()):
+		the_post();
 ?>
-<?php the_title( '<h1>', '</h1>' ); ?>
 <?php
-        the_content();
+		the_title('<h1>', '</h1>');
+?>
+<?php
+		the_content();
 ?>
     </div>
     </div>
@@ -69,14 +71,22 @@ if (have_posts()):
 
     <div id="left-bar-top" class="col-lg-2 col-md-2 col-md-pull-10">
         <nav role="navigation">
-        <div id="MenuSwitch">
-        <input type="checkbox" />
-        <span></span>
-        <span></span>
-        <span></span>
+        <div id="menuToggle">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+            
         <?php
-        $args=array('theme_location' => 'primary');?>
-            <?php wp_nav_menu($args);?>
+		$primaryArgs=array(
+			'theme_location'=>'primary',
+			'menu'=>'primary nav',
+			'menu_id'=>'menu',
+            'container'=>false
+            
+		);
+		wp_nav_menu($primaryArgs);
+?>
         </div>
         </nav>
     </div>
@@ -87,14 +97,18 @@ if (have_posts()):
     <div class="col-lg-4 col-md-4" id="bottom-bar-logo">
             <span id="logo-text">
             <?php
-if (is_active_sidebar('Bottom Corner')) {?>
-    <?php dynamic_sidebar('Bottom Corner');?>
-<?php } //is_active_sidebar('Bottom Corner')
+		if(is_active_sidebar('Bottom Corner')) {
+?>
+    <?php
+			dynamic_sidebar('Bottom Corner');
+?>
+<?php
+		} //is_active_sidebar('Bottom Corner')
 ?></span><!--id=logo-text--> 
         <a href="http://localhost/scarlett-angel-com/"><div id="logo-anchor">
 
             <img id="logo" src="<?php
-echo get_template_directory_uri();
+		echo get_template_directory_uri();
 ?>/theme-images/bottom-corner.png" class="img-responsive img-circle animated-logo">
 
         </div><!--id=logo-anchor--></a>
@@ -104,7 +118,7 @@ echo get_template_directory_uri();
     </div>
 </div>
 <?php
-    endwhile;
+	endwhile;
 ?>
 <?php
 endif;
